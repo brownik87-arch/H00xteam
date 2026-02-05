5b65e7034912dd0109ed1d9824e3c9083c65fd5c
brownik87-arch <brownik87@gmail.com>
2026-02-05 14:01:19 +0500

    Update H00xmenu V2.lua

--- H00xmenu V2.lua
+++ H00xmenu V2.lua
@@ -4,3 +4,17 @@ local LocalPlayer = Players.LocalPlayer
 
 local gui = Instance.new("ScreenGui", game.CoreGui)
 gui.Name = "H00xmenu"
+
+local bubble = Instance.new("TextButton", gui)
+bubble.Size = UDim2.new(0, 60, 0, 60)
+bubble.Position = UDim2.new(0.9, 0, 0.8, 0)
+bubble.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+bubble.Text = "_"
+bubble.TextColor3 = Color3.fromRGB(0, 255, 0)
+bubble.Font = Enum.Font.SourceSansBold
+bubble.TextScaled = true
+Instance.new("UICorner", bubble).CornerRadius = UDim.new(1, 0)
+
+TweenService:Create(bubble, TweenInfo.new(1, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut, -1, true), {
+    BackgroundColor3 = Color3.fromRGB(0, 50, 0)
+}):Play()
