fe2dd9bf47d8d7db52d74c23943209280631c527
brownik87-arch <brownik87@gmail.com>
2026-02-05 14:00:09 +0500

    Update H00xmenu V2.lua

--- H00xmenu V2.lua
+++ H00xmenu V2.lua
@@ -1,2 +1,6 @@
+local Players = game:GetService("Players")
+local TweenService = game:GetService("TweenService")
+local LocalPlayer = Players.LocalPlayer
+
 local gui = Instance.new("ScreenGui", game.CoreGui)
 gui.Name = "H00xmenu"
